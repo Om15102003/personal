96ce942650693465325815408524e66f392c31db
Om15102003 <143651821+Om15102003@users.noreply.github.com>
2024-04-04 03:29:27 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+document.querySelectorAll(".drum")[0].addEventListener("click", function(){
+    var tom1=new Audio("./sound/audio1.opus");
+                tom1.play();
+})
+document.querySelectorAll(".drum")[1].addEventListener("click", function(){
+    var tom2=new Audio("./sound/audio2.opus");
+                tom2.play();
+})
+document.querySelectorAll(".drum")[2].addEventListener("click", function(){
+    var tom3=new Audio("./sound/audio3.opus");
+                tom3.play();
+})
+document.querySelectorAll(".drum")[3].addEventListener("click", function(){
+    var tom4=new Audio("./sound/audio4.opus");
+                tom4.play();
+})
